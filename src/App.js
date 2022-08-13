45731c64b64a4e80149dd174145df1a5e3e5a103
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-12 20:38:59 -0500

    se agrego estilos generales de la pagina y al logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import logo from "./assets/imagenes/stock-photo-cup.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="aplicacion-tareas">
+      <div className="logo-contenedor">
+        <img src={logo}
+          className="logo"
+          alt="logo"
+        />
+      </div>
     </div>
   );
 }
